5b15450bb6578a5fa403e681deb016919d0ba50f
Mostafa-Abedi <79171483+Mostafa-Abedi@users.noreply.github.com>
2024-02-09 17:33:27 -0500

    Update players.c

--- jeopardy_source/players.c
+++ jeopardy_source/players.c
@@ -1,7 +1,7 @@
 /*
  * Tutorial 4 Jeopardy Project for SOFE 3950U / CSCI 3020U: Operating Systems
  *
- * Copyright (C) 2015, <GROUP MEMBERS>
+ * Copyright (C) 2015, <Mostafa Abedi, Nathaniel Manoj, Calvin Reveredo>
  * All rights reserved.
  *
  */
@@ -10,15 +10,39 @@
 #include <string.h>
 #include "players.h"
 
+
+    // Player struct for each player
+
+
 // Returns true if the player name matches one of the existing players
-bool player_exists(player *players, int num_players, char *name)
+bool player_exists(player *players, char *name)
 {
-    return false;
+    bool exists;
+    
+    for(int i = 0; i<= sizeof(players); i++){
+        if (players[i].name == name)
+        {
+            exists = true;
+            break;
+        }
+        else{
+            exists = false;
+        }
+        
+    }
+    return exists;
+    
 }
 
-// Go through the list of players and update the score for the 
-// player given their name
+// Updates the score for that player given their name
 void update_score(player *players, int num_players, char *name, int score)
 {
     
+    for(int i = 1; i <= num_players; i++)
+    {
+        if (players[i].name == name)
+        {
+            players[i].score = score; 
+        }       
+    }
 }
