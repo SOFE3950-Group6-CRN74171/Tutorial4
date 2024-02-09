3357b54b612271ce38c3d99f498ebdd440d53a00
Mostafa-Abedi <79171483+Mostafa-Abedi@users.noreply.github.com>
2024-02-09 17:34:14 -0500

    Update players.h

--- jeopardy_source/players.h
+++ jeopardy_source/players.h
@@ -1,7 +1,7 @@
 /*
  * Tutorial 4 Jeopardy Project for SOFE 3950U / CSCI 3020U: Operating Systems
  *
- * Copyright (C) 2015, <GROUP MEMBERS>
+ * Copyright (C) 2015, <Mostafa Abedi, Nathaniel Manoj, Calvin Reveredo>
  * All rights reserved.
  *
  */
@@ -13,16 +13,15 @@
 #define MAX_LEN 256
 
 // Player struct for each player
-typedef struct {
+typedef struct{
     char name[MAX_LEN];
     int score;
-} player;
+}player;
 
 // Returns true if the player name matches one of the existing players
-extern bool player_exists(player *players, int num_players, char *name);
+extern bool player_exists(player *players, char *name);
 
-// Go through the list of players and update the score for the 
-// player given their name
+// Updates the score for that player given their name
 extern void update_score(player *players, int num_players, char *name, int score);
 
 #endif /* PLAYERS_H_ */
