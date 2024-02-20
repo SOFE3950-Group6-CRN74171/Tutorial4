cf2c6bc074bb2856e8378fb7b17d21257ab79880
Mostafa-Abedi <79171483+Mostafa-Abedi@users.noreply.github.com>
2024-02-19 20:41:53 -0500

    Update questions.c

--- jeopardy_source/questions.c
+++ jeopardy_source/questions.c
@@ -12,25 +12,25 @@
 
 // Array of Questions
 question questions[NUM_QUESTIONS] = {
-    {"Science", "It's the two gases that make up the majority of Earth's atmosphere.", "What are Nitrogen and Oxygen?", 100, false},
-    {"Geography", "This mountain range holds the title for the longest in the world.", "What are the Andes?", 100, false},
-    {"History", "This ancient civilization constructed the Great Pyramids of Giza.", "Who were the Ancient Egyptians?", 100, false},
+    {"Science", "It's the two gases that make up the majority of Earth's atmosphere.", "Nitrogen and Oxygen", 100, false},
+    {"Geography", "This mountain range holds the title for the longest in the world.", "the Andes", 100, false},
+    {"History", "This ancient civilization constructed the Great Pyramids of Giza.", "the Ancient Egyptians", 100, false},
     
-    {"Science", "Plants use this process to make their own food.", "What is Photosynthesis?", 200, false},
-    {"Geography", "Name the country that is both an island and a continent.", "What is Australia?", 200, false},
-    {"History", "She was the first female Prime Minister of the United Kingdom.", "Who was Margaret Thatcher?", 200, false},
+    {"Science", "Plants use this process to make their own food.", "Photosynthesis", 200, false},
+    {"Geography", "Name the country that is both an island and a continent.", "Australia", 200, false},
+    {"History", "She was the first female Prime Minister of the United Kingdom.", "Margaret Thatcher", 200, false},
     
-    {"Science", "This is the smallest bone found in the human body.", "What is the Stapes (in the ear)?", 300, false},
+    {"Science", "This is the smallest bone found in the human body.", "the Stapes (in the ear)", 300, false},
     {"Geography", "Identify the largest ocean by surface area.", "What is the Pacific Ocean?", 300, false},
     {"History", "This British monarch holds the record for the longest reign.", "Who was Queen Victoria?", 300, false},
     
-    {"Science", "The process by which liquid water turns into water vapor.", "What is Evaporation?", 400, false},
-    {"Geography", "Name the capital city of Brazil.", "What is Brasília?", 400, false},
-    {"History", "He led the first expedition to successfully circumnavigate the globe.", "Who was Ferdinand Magellan?", 400, false},
+    {"Science", "The process by which liquid water turns into water vapor.", "Evaporation", 400, false},
+    {"Geography", "Name the capital city of Brazil.", "Brasília", 400, false},
+    {"History", "He led the first expedition to successfully circumnavigate the globe.", "Ferdinand Magellan", 400, false},
     
-    {"Science", "This scientific field studies fossils.", "What is Paleontology?", 500, false},
-    {"Geography", "Identify the sea that doesn't have any coastlines.", "What is the Sargasso Sea?", 500, false},
-    {"History", "This global conflict lasted from 1939 to 1945.", "What is World War II?", 500, false},
+    {"Science", "This scientific field studies fossils.", "Paleontology", 500, false},
+    {"Geography", "Identify the sea that doesn't have any coastlines.", "the Sargasso Sea", 500, false},
+    {"History", "This global conflict lasted from 1939 to 1945.", "World War II", 500, false},
 };
 
 // Displays each of the remaining categories and question dollar values that have not been answered
