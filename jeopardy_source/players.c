8a7d9cc216ff0818956b94583ba138875b77153f
Mostafa-Abedi <79171483+Mostafa-Abedi@users.noreply.github.com>
2024-02-19 19:31:45 -0500

    Update players.c

--- jeopardy_source/players.c
+++ jeopardy_source/players.c
@@ -10,39 +10,37 @@
 #include <string.h>
 #include "players.h"
 
-
-    // Player struct for each player
-
-
 // Returns true if the player name matches one of the existing players
-bool player_exists(player *players, char *name)
-{
-    bool exists;
-    
-    for(int i = 0; i<= sizeof(players); i++){
-        if (players[i].name == name)
-        {
-            exists = true;
-            break;
+bool player_exists(player *players, int num_players, char *name){
+    for(int i=0; i < num_players; i++){
+        if(strcmp(players[i].name, name) == 0){
+            return true;
         }
-        else{
-            exists = false;
-        }
-        
     }
-    return exists;
     
+    return false;
 }
 
-// Updates the score for that player given their name
-void update_score(player *players, int num_players, char *name, int score)
-{
-    
-    for(int i = 1; i <= num_players; i++)
-    {
-        if (players[i].name == name)
-        {
-            players[i].score = score; 
-        }       
+// Go through the list of players and update the score for the 
+// player given their name
+void update_score(player *players, int num_players, char *name, int score){
+    for(int i=0; i < num_players; i++){
+        if(strcmp(players[i].name, name) == 0){
+            players[i].score += score;
+        }
     }
+}
+
+void highest_score(player *players, int num_players){
+    int highest_score = 0;
+    char *winner;
+    
+    for(int i=0; i < num_players; i++){
+        if(players[i].score > highest_score){
+            highest_score = players[i].score;
+            winner = players[i].name;
+        }
+    }
+    
+    printf("\n\nThe Winner of this Game, with an astonishing $%d is %s\n", highest_score, winner);
 }
